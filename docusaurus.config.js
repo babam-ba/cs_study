589804d117cdee285dd0f5aee6087e5c71cc0eab
juyeong-chungbuk <happyu9986@gmail.com>
2023-01-26 21:10:43 +0900

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,103 @@
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+(
+  module.exports = {
+    title: "Babamba",
+    tagline: "Dinosaurs are cool",
+    url: "https://babam-ba.github.io",
+    baseUrl: "/cs_study/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
+    organizationName: "babam-ba",
+    projectName: "cs_study",
+
+    presets: [
+      [
+        "@docusaurus/preset-classic",
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            sidebarPath: require.resolve("./sidebars.js"),
+            // Please change this to your repo.
+            editUrl: "https://github.com/babam-ba/cs_study/edit/main/website/",
+          },
+          blog: {
+            showReadingTime: true,
+            // Please change this to your repo.
+            editUrl:
+              "https://github.com/babam-ba/cs_study/edit/main/website/blog/",
+          },
+          theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+          },
+        }),
+      ],
+    ],
+
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+        navbar: {
+          title: "My Site",
+          logo: {
+            alt: "My Site Logo",
+            src: "img/logo.svg",
+          },
+          items: [
+            {
+              type: "doc",
+              docId: "intro",
+              position: "left",
+              label: "Tutorial",
+            },
+            { to: "/blog", label: "Blog", position: "left" },
+            {
+              href: "https://github.com/babam-ba/cs_study",
+              label: "GitHub",
+              position: "right",
+            },
+          ],
+        },
+        footer: {
+          style: "dark",
+          links: [
+            {
+              title: "Docs",
+              items: [
+                {
+                  label: "Tutorial",
+                  to: "/docs/intro",
+                },
+              ],
+            },
+            {
+              title: "Community",
+              items: [],
+            },
+            {
+              title: "More",
+              items: [
+                {
+                  label: "Blog",
+                  to: "/blog",
+                },
+                {
+                  label: "GitHub",
+                  href: "https://github.com/babam-ba/cs_study",
+                },
+              ],
+            },
+          ],
+          copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        },
+        prism: {
+          theme: lightCodeTheme,
+          darkTheme: darkCodeTheme,
+        },
+      }),
+  }
+);
